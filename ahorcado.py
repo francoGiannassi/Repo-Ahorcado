8af01ac36aa67eafab21d90944d2be7642f41d30
frvnnko <49121283+francoGiannassi@users.noreply.github.com>
2021-06-14 19:08:09 -0300

    terminado 1er sprint

--- ahorcado.py
+++ ahorcado.py
@@ -2,6 +2,9 @@ class Ahorcado:
 
     nombreUsuario = None
     palabra = "Ahorcado"
+    letrasCorrectas = []
+    letrasIncorrectas = []
+    intentosFallidos = 0
 
     def getNombreUsuario(self):
         return self.nombreUsuario
@@ -11,3 +14,54 @@ class Ahorcado:
 
     def getPalabra(self):
         return self.palabra
+
+    def ingresaLetra(self, letra):
+        if letra == "" or not letra.isalpha():
+            self.letrasIncorrectas.append(letra)
+        elif self.esLetraCorrecta(letra):
+            self.letrasCorrectas.append(letra.lower())
+            self.letrasCorrectas.append(letra.upper())
+        else:
+            self.letrasIncorrectas.append(letra.lower())
+            self.letrasIncorrectas.append(letra.upper())
+            self.sumarIntentoFallido()
+        
+    def getLetrasCorrectas(self):
+        return self.letrasCorrectas
+
+    def getLetrasIncorrectas(self):
+        return self.letrasIncorrectas
+
+    def esLetraCorrecta(self, letra):
+        if letra.lower() in self.palabra:
+            return True
+        else:
+            return False
+        
+    def getPalabraParcial(self):
+        palabraParcial = ""
+        for letra in self.palabra:
+            if letra in self.letrasCorrectas:
+                palabraParcial += letra.upper()
+            else: 
+                palabraParcial += "-"
+        return palabraParcial
+    
+    def sumarIntentoFallido(self):
+        self.intentosFallidos+=1
+    
+    def getIntentosFallidos(self):
+        return self.intentosFallidos
+
+    def getEstadoFinalJuego(self):
+        if self.intentosFallidos == 7:
+            return "Perdido"
+        elif self.esPalabraFinal():
+            return "Ganado"
+    
+    def esPalabraFinal(self):
+        esPalabra = True
+        for letra in self.palabra:
+            if letra not in self.letrasCorrectas:
+                esPalabra = False
+        return esPalabra
--- test_ahorcado.py
+++ test_ahorcado.py
@@ -18,8 +18,90 @@ class TestsConfiguracion(unittest.TestCase):
         ahorcado = Ahorcado()
         self.assertIn("Ahorcado", ahorcado.getPalabra())
 
-class TestsJuego(unittest.TestCase):
+class TestsIngresoLetra(unittest.TestCase):
 
     def test_valida_letra_correcta(self):
         ahorcado = Ahorcado()
-        self.assertRegex(ahorcado.getPalabra(),"a")
+        ahorcado.ingresaLetra("a")
+        self.assertIn("a",ahorcado.getLetrasCorrectas())
+    
+    def test_valida_letra_incorrecta(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("e")
+        self.assertIn("e",ahorcado.getLetrasIncorrectas())
+
+    def test_valida_letra_mayuscula(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("H")
+        self.assertIn("H",ahorcado.getLetrasCorrectas())
+
+    def test_valida_caracter_no_alfabetico(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("$")
+        self.assertIn("$",ahorcado.getLetrasIncorrectas())
+    
+    def test_valida_caracter_vacio(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("")
+        self.assertIn("",ahorcado.getLetrasIncorrectas())
+
+    def test_es_letra_correcta(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("c")
+        self.assertTrue(ahorcado.esLetraCorrecta("c"))
+    
+    def test_es_letra_incorrecta(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("t")
+        self.assertFalse(ahorcado.esLetraCorrecta("t"))
+    
+    def test_palabra_parcial_correcta(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("A")
+        ahorcado.ingresaLetra("H")
+        ahorcado.ingresaLetra("C")
+        ahorcado.ingresaLetra("D")
+        self.assertEqual(ahorcado.getPalabraParcial(),"AH--CAD-")
+    
+    def test_valida_conteo_intentos_fallidos(self):
+        ahorcado = Ahorcado()
+        ahorcado.ingresaLetra("f")
+        ahorcado.ingresaLetra("i")
+        ahorcado.ingresaLetra("m")
+        self.assertEqual(ahorcado.getIntentosFallidos(),3)
+
+    def test_valida_tope_maximo_intentos_fallidos(self):
+        ahorcado1 = Ahorcado()
+        ahorcado1.ingresaLetra("f")
+        ahorcado1.ingresaLetra("i")
+        ahorcado1.ingresaLetra("m")
+        ahorcado1.ingresaLetra("u")
+        ahorcado1.ingresaLetra("j")
+        ahorcado1.ingresaLetra("k")
+        ahorcado1.ingresaLetra("p")
+        self.assertEqual(ahorcado1.getIntentosFallidos(),7)
+
+class TestsResultadoFinal(unittest.TestCase):
+
+    def test_valida_partida_perdida(self):
+        ahorcado2 = Ahorcado()
+        ahorcado2.ingresaLetra("f")
+        ahorcado2.ingresaLetra("i")
+        ahorcado2.ingresaLetra("m")
+        ahorcado2.ingresaLetra("u")
+        ahorcado2.ingresaLetra("j")
+        ahorcado2.ingresaLetra("k")
+        ahorcado2.ingresaLetra("p")
+        self.assertEqual("Perdido",ahorcado2.getEstadoFinalJuego())
+
+    def test_valida_partida_ganada(self):
+        ahorcado3 = Ahorcado()
+        ahorcado3.ingresaLetra("a")
+        ahorcado3.ingresaLetra("h")
+        ahorcado3.ingresaLetra("o")
+        ahorcado3.ingresaLetra("r")
+        ahorcado3.ingresaLetra("c")
+        ahorcado3.ingresaLetra("d")
+        self.assertEqual("Ganado",ahorcado3.getEstadoFinalJuego())
+
+
